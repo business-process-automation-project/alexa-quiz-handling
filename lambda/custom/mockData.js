bfce189d1db2f75bc4789b14a512c91a7b39a2dc
Florian Fricke <florian_fricke@gmx.de>
2019-05-17 14:21:10 +0200

    add mock data

---
+++ lambda/custom/mockData.js
@@ -0,0 +1,59 @@
+exports.mockData = [
+    {
+        "Question": "Was ist meine Lieblingsfrage?",
+        "Answers":
+        [
+            "Rot",
+            "Blau",
+            "Grün"
+        ],
+        "Correctness":
+        [
+            false,
+            true,
+            false
+        ]
+    },
+    {
+        "Question": "Wie heiße ich mit Vornamen?",
+        "Answers": [
+            "Norbert",
+            "Horst",
+            "Florian"
+        ],
+        "Correctness":
+        [
+            false,
+            false,
+            true
+        ]
+    },
+    {
+        "Question": "Wie heißt der Vater von Donald Duck?",
+        "Answers": [
+            "Degenhard",
+            "Jimmy",
+            "Alfred"
+        ],
+        "Correctness":
+        [
+            true,
+            false,
+            false
+        ]
+    },
+    {
+        "Question": "Wie oft schluckt ein erwachsener Mensch am Tag?",
+        "Answers": [
+            "sechshundertmal",
+            "fünfzigmal",
+            "achthundertmal"
+        ],
+        "Correctness":
+        [
+            true,
+            false,
+            false
+        ]
+    }
+]
