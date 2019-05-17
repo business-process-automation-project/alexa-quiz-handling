5bd30fd340f02ca725857bb556ec56ddf22a85da
Florian Fricke <florian_fricke@gmx.de>
2019-05-17 16:46:32 +0200

    add GameHandler (wip)

---
+++ lambda/custom/index.js
@@ -0,0 +1,80 @@
+const Alexa = require('ask-sdk-core');
+const language = require('./language');
+const StartGameIntent = require('./startGameHandler');
+
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+  },
+  handle(handlerInput) {
+    
+    return handlerInput.responseBuilder
+      .speak(language.deData.translation.WELCOME_MESSAGE)
+      .reprompt(language.deData.translation.REPROMT_MESSAGE)
+      .getResponse();
+  },
+};
+
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+      .speak(language.deData.translation.HELP_MESSAGE)
+      .reprompt(language.deData.translation.REPROMT_MESSAGE)
+      .getResponse();
+  },
+};
+
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+      .speak(language.deData.translation.CLOSE_MESSAGE)
+      .getResponse();
+  },
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+      .speak(language.deData.translation.CLOSE_MESSAGE)
+      .getResponse();
+  },
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak(language.deData.translation.ERROR_MESSAGE)
+      .reprompt(language.deData.translation.ERROR_MESSAGE)
+      .getResponse();
+  },
+};
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    StartGameIntent.StartGameHandler,
+    HelpIntentHandler,
+    CancelAndStopIntentHandler,
+    SessionEndedRequestHandler
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
