a54e8052ecafed65dee4fac5b63e2802d977dba4
Florian Fricke <florian_fricke@gmx.de>
2019-06-14 21:16:29 +0200

    add getQuestion from database

--- lambda/custom/startGameHandler.js
+++ lambda/custom/startGameHandler.js
@@ -1,6 +1,5 @@
-const https = require('https');
-const mockData = require('./mockData')
 const language = require('./language');
+const mqtt = require('mqtt')
 
 exports.StartGameHandler = {
     canHandle(handlerInput) {
@@ -8,76 +7,76 @@ exports.StartGameHandler = {
             && handlerInput.requestEnvelope.request.intent.name === 'StartGameIntent';
     },
     async handle(handlerInput) {
-        const filledSlots = handlerInput.requestEnvelope.request.intent.slots;
-        const NumberOfQuestions = filledSlots.NumberofQuestions.value;
+        // const filledSlots = handlerInput.requestEnvelope.request.intent.slots;
+        // const NumberOfQuestions = filledSlots.NumberofQuestions.value;
 
-        if (typeof NumberOfQuestions === 'undefined' || NumberOfQuestions === '?' || NumberOfQuestions < 1) {
-            NumberOfQuestions = 5;
-        }
+        // if (typeof NumberOfQuestions === 'undefined' || NumberOfQuestions === '?' || NumberOfQuestions < 1) {
+        //     NumberOfQuestions = 3;
+        // }
 
-        // const data = await getRecords(NumberOfQuestions);
-        
-        let speechText;
-        let letters = ['A','B','C']
-        speechText = language.deData.translation.START_MESSAGE + '<break time=".5s"/>';
-        for (let i = 0; i < NumberOfQuestions; i++) {
-            speechText += 'Frage ' + (i + 1) + '<break time=".2s"/>' + mockData.mockData[i].Question + '?';
-            for (let j = 0; j < 3; j++) {
-                speechText += '<break time=".4s"/>'+ letters[j] + '<break time=".2s"/>' + mockData.mockData[i].Answers[j];
-            }
-            speechText += '<break time=".8s"/>';
-        }
+        // publishDataMQTT("Lights", "DefaultLight");
+        let data = await mqttConnection();
 
-        speechText += language.deData.translation.CLOSE_MESSAGE;
+        let speechText = '';
+        speechText += 'Die Frage lautet' + '<break time=".2s"/> ' + data[0].Question + '? ';
+        speechText += '<break time=".4s"/>' + 'Ist es eins' + '<break time=".2s"/>' + data[0].Answer[0].Text + "?";
+        speechText += '<break time=".4s"/>' + 'zwei' + '<break time=".2s"/>' + data[0].Answer[1].Text + "?";
+        speechText += '<break time=".4s"/>' + 'oder ist es drei' + '<break time=".2s"/>' + data[0].Answer[2].Text + "?";
+        speechText += '<break time="2s"/> ';
+        speechText += 'Kann es los gehen?';
+
+        // for (let i = 0; i < NumberOfQuestions; i++) {
+        //     speechText += 'Frage ' + (i + 1) + '<break time=".2s"/> ' + data[i].Question + '? ';
+        //     speechText += '<break time=".4s"/>' + 'Ist es eins' + '<break time=".2s"/>' + data[i].Answer[0].Text;
+        //     speechText += '<break time=".4s"/>' + 'zwei' + '<break time=".2s"/>' + data[i].Answer[1].Text;
+        //     speechText += '<break time=".4s"/>' + 'oder ist es drei' + '<break time=".2s"/>' + data[i].Answer[2].Text;
+        //     speechText += '<break time=".8s"/> ';
+        // }
 
         return handlerInput.responseBuilder
             .speak(speechText)
+            .withShouldEndSession(false)
             .getResponse();
+
+        // publishDataMQTT("Monitor",'{Question:"Wie gehts?", Answer1: "gut", Answer2: "schlecht", Answer3: "normal"}');
+
     }
 };
 
-function getRecords(recType, ticketNumbers, timespan) {
-    const hdrAuth = "Bearer " // + accessToken;
-    let sort = 'DESC';
+function publishDataMQTT(channelName, status) {
+    const client = mqtt.connect('mqtt://34.230.40.176');
 
-    if (timespan === "ältesten" || timespan === "älteste" || timespan === "spätesten" || timespan === "spätesteste") {
-        sort = 'ASC';
-    }
-
-    return new Promise(((resolve, reject) => {
-        const serviceNowInstance = constants.servicenow.instance;
-
-        const options = {
-            hostname: serviceNowInstance,
-            port: 443,
-            path: '/api/now/table/' + recType + '?sysparm_query=ORDERBY' + sort + 'sys_updated_on&sysparm_limit=' + ticketNumbers,
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: hdrAuth
+    client.on('connect', function () {
+        console.log("connected with mqtt: " + channelName );
+        client.subscribe(channelName, function (err) {
+            if (!err) {
+                client.publish(channelName, status);
             }
-        };
+        })
+        client.end();
+    })
+}
 
-        const request = https.request(options, (response) => {
-            response.setEncoding('utf8');
-            let returnData = '';
+function mqttConnection() {
+    const client = mqtt.connect('mqtt://34.230.40.176');
+    
+    return new Promise(((resolve, reject) => { 
+        client.on('connect', function () {
+            console.log("connected with mqtt");
+            client.subscribe("RequestQuestions", function (err) {
+                if (!err) {
+                    client.publish("RequestQuestions", "1");
+                } else {
+                    reject(err);
+                }
+            })
+            client.unsubscribe("RequestQuestions");
+            client.subscribe("ResponseQuestions");
+        })
 
-            if (response.statusCode < 200 || response.statusCode >= 300) {
-                return reject(new Error(`${response.statusCode}: ${response.req.getHeader('host')} ${response.req.path}`));
-            }
-
-            response.on('data', (chunk) => {
-                returnData += chunk;
-            });
-
-            response.on('end', () => {
-                resolve(JSON.parse(returnData));
-            });
-
-            response.on('error', (error) => {
-                reject(error);
-            });
-        });
-        request.end();
+        client.on('message', function (topic, message) {
+            resolve(JSON.parse(message));
+            client.end();
+        })
     }));
 }
