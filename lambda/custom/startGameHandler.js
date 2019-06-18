438d23cd429cf83d3b03e1ef106d4c91cc20f6e2
Florian Fricke <florian_fricke@gmx.de>
2019-06-18 15:23:24 +0200

    add global variable for data from db

--- lambda/custom/startGameHandler.js
+++ lambda/custom/startGameHandler.js
@@ -7,38 +7,25 @@ exports.StartGameHandler = {
             && handlerInput.requestEnvelope.request.intent.name === 'StartGameIntent';
     },
     async handle(handlerInput) {
-        // const filledSlots = handlerInput.requestEnvelope.request.intent.slots;
-        // const NumberOfQuestions = filledSlots.NumberofQuestions.value;
-
-        // if (typeof NumberOfQuestions === 'undefined' || NumberOfQuestions === '?' || NumberOfQuestions < 1) {
-        //     NumberOfQuestions = 3;
-        // }
 
         // publishDataMQTT("Lights", "DefaultLight");
-        let data = await mqttConnection();
+
+        global.globalData = await mqttConnection();
+
+        // publishDataMQTT("Monitor", JSON.stringify(globalData));
 
         let speechText = '';
-        speechText += 'Die Frage lautet' + '<break time=".2s"/> ' + data[0].Question + '? ';
-        speechText += '<break time=".4s"/>' + 'Ist es eins' + '<break time=".2s"/>' + data[0].Answer[0].Text + "?";
-        speechText += '<break time=".4s"/>' + 'zwei' + '<break time=".2s"/>' + data[0].Answer[1].Text + "?";
-        speechText += '<break time=".4s"/>' + 'oder ist es drei' + '<break time=".2s"/>' + data[0].Answer[2].Text + "?";
+        speechText += 'Die Frage lautet' + '<break time=".2s"/> ' + globalData[0].Question + '? ';
+        speechText += '<break time=".4s"/>' + 'Ist es eins' + '<break time=".2s"/>' + globalData[0].Answer[0].Text + "?";
+        speechText += '<break time=".4s"/>' + 'zwei' + '<break time=".2s"/>' + globalData[0].Answer[1].Text + "?";
+        speechText += '<break time=".4s"/>' + 'oder ist es drei' + '<break time=".2s"/>' + globalData[0].Answer[2].Text + "?";
         speechText += '<break time="2s"/> ';
         speechText += 'Kann es los gehen?';
-
-        // for (let i = 0; i < NumberOfQuestions; i++) {
-        //     speechText += 'Frage ' + (i + 1) + '<break time=".2s"/> ' + data[i].Question + '? ';
-        //     speechText += '<break time=".4s"/>' + 'Ist es eins' + '<break time=".2s"/>' + data[i].Answer[0].Text;
-        //     speechText += '<break time=".4s"/>' + 'zwei' + '<break time=".2s"/>' + data[i].Answer[1].Text;
-        //     speechText += '<break time=".4s"/>' + 'oder ist es drei' + '<break time=".2s"/>' + data[i].Answer[2].Text;
-        //     speechText += '<break time=".8s"/> ';
-        // }
 
         return handlerInput.responseBuilder
             .speak(speechText)
             .withShouldEndSession(false)
             .getResponse();
-
-        // publishDataMQTT("Monitor",'{Question:"Wie gehts?", Answer1: "gut", Answer2: "schlecht", Answer3: "normal"}');
 
     }
 };
